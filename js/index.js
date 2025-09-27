b216e1532be5d2118cd7f2d03c1655ec563cdcbd
HannaDevops <115883036+HannaDevops@users.noreply.github.com>
2025-09-27 21:09:14 +0100

    add js to selector

--- js/index.js
+++ js/index.js
@@ -16,17 +16,37 @@ let berlinTime = berlin.querySelector(".time");
 let berlinToday = moment().tz("Europe/Berlin");
 
 
-
+if(sydney){
 sydneyDate.innerHTML = sydneyToday.format("MMMM Do, YYYY");
 sydneyTime.innerHTML = sydneyToday.format("hh:mm:ss  [<small>]A[</small>]");
-
+}
+if(istanbul){
 istanbulDate.innerHTML = istanbulToday.format("MMMM Do, YYYY");
 istanbulTime.innerHTML = istanbulToday.format("hh:mm:ss  [<small>]A[</small>]");
-
+}
+if(berlin){
 berlinDate.innerHTML = berlinToday.format("MMMM Do, YYYY");
 berlinTime.innerHTML = berlinToday.format("hh:mm:ss  [<small>]A[</small>]");
+}
+}
 
+function updateSelectcity(event){
+    let selectcityZone = event.target.value;
+    let selectcityName = selectcityZone.split("/")[1] ;
+    let selectcityTime = moment().tz(selectcityZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+                <div>
+                    <h2>${selectcityName}</h2>
+                    <div class="date">${selectcityTime.format("MMMM Do YYYY")}</div>
+                </div>
+                <div class="time">${selectcityTime.format("hh:mm:ss  [<small>]A[</small>]")}</div>
+            </div>`;
 }
 
 updateforSecond();
-setInterval(updateforSecond,1000)
+setInterval(updateforSecond,1000);
+
+let selectcityElement = document.querySelector("#selectcity");
+selectcityElement.addEventListener("change", updateSelectcity)
